ac96e4f2877bdc1398931388432ada8c426d6ce2
Mark Disterhof <markdisterhof@outlook.de>
2021-11-03 17:50:26 +0100

    change to output vlen=1

--- Simulation/gr-nr_sync/python/rgrid_c.py
+++ Simulation/gr-nr_sync/python/rgrid_c.py
@@ -11,24 +11,29 @@ import numpy
 from gnuradio import gr
 from nr_phy_sync import nrSSB
 
+
 class rgrid_c(gr.sync_block):
     """
     docstring for block rgrid_c
     """
 
-    def __init__(self, N_RB, N_ID1, N_ID2, k_ssb, mu, f, pbch_data, shared_spectr, paired_spectr):
+    def __init__(self, n_carr, N_ID1, N_ID2, k_ssb, mu, f, pbch_data, shared_spectr, paired_spectr):
         gr.sync_block.__init__(self,
                                name="rgrid_c",
                                in_sig=None,
-                               out_sig=[(numpy.complex64, N_RB*12)])
-        self.resource_grid = numpy.array(nrSSB.get_sync_resource_grid(
-            N_RB, N_ID1, N_ID2, k_ssb, mu, f, shared_spectr, paired_spectr), dtype=numpy.complex64)
+                               out_sig=[(numpy.complex64, (1,))])
+                               
+        self.resource_grid = nrSSB.grid(n_carr, N_ID1, N_ID2, k_ssb, mu, f, shared_spectr, paired_spectr, pbch_data).flatten(order='F')#numpy.array(range(n_carr),dtype=complex)
+        numpy.save('/home/mark/ssb.npy', self.resource_grid)
         self.idx = 0
+        #self.set_output_multiple(n_carr)
 
     def work(self, input_items, output_items):
         out = output_items[0]
-        # <+signal processing here+>
-        out[:] = self.resource_grid[:, self.idx]
-        self.idx = (self.idx + 1) % len(self.resource_grid[0])
-        #
-        return len(output_items[0])
+        n_items = len(out)
+        output_items[0][:] = numpy.take(self.resource_grid,
+                         range(self.idx, self.idx+n_items),
+                         mode='wrap').reshape((n_items, 1))
+        self.idx += n_items
+        self.idx %= len(self.resource_grid)
+        return n_items  # len(out)
